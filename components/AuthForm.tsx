05ccab381966d2be914db246fa7792ce3def2e77
Rana5539 <wa852094@gmail.com>
2024-07-09 07:11:57 +0500

    sign in form

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -3,8 +3,45 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React, { useState } from 'react'
 
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+
+const formSchema = z.object({
+    email: z.string().email(),
+    
+  })
+
+
 const AuthForm = ({type}: {type: string}) => {
     const [user,setUser] = useState(null)
+
+      // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
   return (
     <section className='auth-form '>
         <header className='flex flex-col gap-5 md:gap-8'>
@@ -18,6 +55,7 @@ const AuthForm = ({type}: {type: string}) => {
         <div className='flex flex-col gap-1 md:gap-3'>
 <h1 className='text-24 lg:text-36 font-semibold  text-gray-900'>
    {user? 'Link Account': type==='sign-in'? 'Sign-in': 'Sign-up'} 
+
 </h1>
 <p className='text-16 font-normal text-gray-600 '>
     {user? 'Link you account': 'Please enter your details'}
@@ -29,7 +67,31 @@ const AuthForm = ({type}: {type: string}) => {
 
             </div>
         ): <>
-        FORM
+        <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <div className='form-item'>
+             <FormLabel className='form-label'>
+                Email
+             </FormLabel>
+             <div className='flex w-full flex-col'>
+               <FormControl>
+                <Input placeholder='Enter Your Email' className='input-calss' {...field}/>
+               </FormControl>
+               <FormMessage className='form-message mt-2'>
+
+               </FormMessage>
+             </div>
+            </div>
+          )}
+        />
+        
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
         </>}
         </section>
   )
